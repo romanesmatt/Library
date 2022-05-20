4d382b876d9dbd09aea4fa7364003ec1759d3b5d
Matt Romanes <48772682+romanesmatt@users.noreply.github.com>
2022-05-20 14:39:21 +1200

    Initial commit

---
+++ src/LibraryModel.java
@@ -0,0 +1,71 @@
+/*
+ * LibraryModel.java
+ * Author:
+ * Created on:
+ */
+
+
+
+import javax.swing.*;
+
+public class LibraryModel {
+
+    // For use in creating dialogs and making them modal
+    private JFrame dialogParent;
+
+    public LibraryModel(JFrame parent, String userid, String password) {
+	dialogParent = parent;
+    }
+
+    public String bookLookup(int isbn) {
+	return "Lookup Book Stub";
+    }
+
+    public String showCatalogue() {
+	return "Show Catalogue Stub";
+    }
+
+    public String showLoanedBooks() {
+	return "Show Loaned Books Stub";
+    }
+
+    public String showAuthor(int authorID) {
+	return "Show Author Stub";
+    }
+
+    public String showAllAuthors() {
+	return "Show All Authors Stub";
+    }
+
+    public String showCustomer(int customerID) {
+	return "Show Customer Stub";
+    }
+
+    public String showAllCustomers() {
+	return "Show All Customers Stub";
+    }
+
+    public String borrowBook(int isbn, int customerID,
+			     int day, int month, int year) {
+	return "Borrow Book Stub";
+    }
+
+    public String returnBook(int isbn, int customerid) {
+	return "Return Book Stub";
+    }
+
+    public void closeDBConnection() {
+    }
+
+    public String deleteCus(int customerID) {
+    	return "Delete Customer";
+    }
+
+    public String deleteAuthor(int authorID) {
+    	return "Delete Author";
+    }
+
+    public String deleteBook(int isbn) {
+    	return "Delete Book";
+    }
+}
