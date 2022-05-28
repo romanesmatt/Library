fe9af76821ce001aae143e052003d8ea677838bf
romanesmatt <mattromanes@gmail.com>
2022-05-28 17:32:45 +1200

    Complete bookLookup method

--- src/LibraryModel.java
+++ src/LibraryModel.java
@@ -27,18 +27,61 @@ public class LibraryModel {
         this.dialogParent = parent;
         try{
             Class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://db.ecs.vuw.ac.nz" + userId + "_jdbc";
+
+            String url = "jdbc:postgresql://db.ecs.vuw.ac.nz" + userId + "_jdbc"; //URL for running project on uni lab machines
             connection = DriverManager.getConnection(url, userId, password);
 
             System.out.println("Connection established.");
 
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
+            System.out.println("Connection cannot be established.");
+            e.printStackTrace();
+        }catch(ClassNotFoundException e){
+            System.out.println("Class not found.");
             e.printStackTrace();
         }
     }
 
+    /**
+     * Prints out the information of a given book by searching its ISBN.
+     * Goes through the library of books using a query, and if it finds a match,
+     * it will be addd onto the output and then printed.
+     * @param isbn
+     * @return
+     */
     public String bookLookup(int isbn) {
-	return "Lookup Book Stub";
+
+//        Declaring variables for accessibility throughout method
+        String edition = "";
+        String nCopies = "";
+        String copiesLeft = "";
+        String author = "";
+        String title = "";
+
+
+        try{
+            String query = "SELECT * FROM Book NATURAL JOIN Book_Author NATURAL JOIN AUTHOR " +
+                    "WHERE (isbn = " + isbn + ")" +
+                    "ORDER BY AuthorSeqNo ASC;";
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            //Searching for the result then adding it to the output.
+            while(resultSet.next()){
+                edition = resultSet.getString("edition_no");
+                nCopies =resultSet.getString("numofcop");
+                copiesLeft = resultSet.getString("numleft");
+                author = resultSet.getString("surname") + ",";
+                title = resultSet.getString("Title");
+            }
+            statement.close();
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return "Book Lookup: \n 	" + isbn + ": " + title + "\n 	Edition: " + edition +
+                " - Number of copies: " + nCopies + " - Copies Left: " + copiesLeft + "\n 	Authors: " + author.replaceAll("\\s+"," ");
     }
 
     public String showCatalogue() {
